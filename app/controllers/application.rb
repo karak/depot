d277de225d59452ba0d22eca863a86099076a216
yasushi <yasushi@andLinux.(none)>
2009-11-20 07:23:50 -0500

    fix InvalidAuthentification to rails2.0

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => 'f337e674500058432d9c5f4ad908d27d'
+  protect_from_forgery :secret => 'f337e674500058432d9c5f4ad908d27d'
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
