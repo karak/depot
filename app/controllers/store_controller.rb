c96f143fd99b84cfdf06c91b7cc063738400e0cf
yasushi <yasushi@andLinux.(none)>
2009-11-20 11:39:19 -0500

    close iteration D-1
    move cart to side-bar.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,7 @@
 class StoreController < ApplicationController
   def index
     @products = Product.find_products_for_sale
+    @cart = find_cart
   end
   
   def add_to_cart
@@ -13,6 +14,7 @@ class StoreController < ApplicationController
     else
       @cart = find_cart
       @cart.add_product(product)
+      redirect_to_index
     end
   end
   
@@ -26,8 +28,8 @@ class StoreController < ApplicationController
     session[:cart] ||= Cart.new
   end
   
-  def redirect_to_index(msg)
-    flash[:notice] = msg
+  def redirect_to_index(msg = nil)
+    flash[:notice] = msg if msg
     redirect_to :action => :index
   end
 
